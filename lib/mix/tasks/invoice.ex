9d5a858acdcbbe6bdfa5f95a6568b8f7ee2a9e1a
Lukas Rieder <l.rieder@gmail.com>
2018-04-05 12:21:55 +0200

    allow for vat exempted invoices

--- lib/mix/tasks/invoice.ex
+++ lib/mix/tasks/invoice.ex
@@ -7,7 +7,7 @@ defmodule Mix.Tasks.Invoice do
         args,
         switches: [
           for: [:string],
-          number: [:integer],
+          number: [:string],
           date: [:string],
           due_days: [:integer]
         ]
@@ -20,6 +20,7 @@ defmodule Mix.Tasks.Invoice do
     |> build_time_entries
     |> build_time_entry_groups
     |> build_invoice
+    |> build_invoice_total
     |> write_invoice
   end
 
@@ -34,6 +35,7 @@ defmodule Mix.Tasks.Invoice do
   def build_recipient(%{customer: %{id: id}} = state) do
     recipient = File.read!("priv/recipients/customer_#{id}.json")
                 |> Poison.Parser.parse!(keys: :atoms)
+    recipient = Map.put(recipient, :vat_exempt, Map.get(recipient, :vat_exempt, false))
     Map.put(state, :recipient, recipient)
   end
 
@@ -48,20 +50,26 @@ defmodule Mix.Tasks.Invoice do
   end
 
   def build_invoice(state) do
-    line_total = sum_revenue(state.time_entries)
-    tax_total = taxes(line_total)
-    total = Decimal.add(line_total, tax_total)
     Map.put(state, :invoice, %{
       no: state.number,
       date: state.date,
       from: Timex.parse!(state.from, "%Y-%m-%d", :strftime),
       to: Timex.parse!(state.to, "%Y-%m-%d", :strftime),
       due_days: state.due_days,
-      due_date: Timex.shift(state.date, days: state.due_days),
-      line_total: line_total,
-      tax_total: tax_total,
-      total: total
+      due_date: Timex.shift(state.date, days: state.due_days)
     })
+  end
+
+  def build_invoice_total(%{recipient: recipient, invoice: invoice} = state) do
+    line_total = sum_revenue(state.time_entries)
+    tax_total = unless recipient.vat_exempt do
+      taxes(line_total)
+    else
+      Decimal.new(0)
+    end
+    total = Decimal.add(line_total, tax_total)
+    invoice = Map.merge(invoice, %{line_total: line_total, tax_total: tax_total, total: total})
+    %{state | invoice: invoice}
   end
 
   def sum_revenue(time_entries) do
